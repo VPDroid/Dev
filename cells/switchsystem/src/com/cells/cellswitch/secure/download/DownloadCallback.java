018e24c7e651a5802aea8dccfec6f02b41b0064f
VPDroid <74720747+VPDroid@users.noreply.github.com>
2020-11-21 19:14:00 +0800

    Update DownloadCallback.java

--- cells/switchsystem/src/com/cells/cellswitch/secure/download/DownloadCallback.java
+++ cells/switchsystem/src/com/cells/cellswitch/secure/download/DownloadCallback.java
@@ -11,21 +11,21 @@ public interface DownloadCallback {
     void onSuccess(File file);
 
     /**
-     * 下载失败
+     * 
      *
      * @param e
      */
     void onFailure(Exception e);
 
     /**
-     * 下载进度
+     * 
      *
      * @param progress
      */
     void onProgress(long progress, long currentLength);
 
     /**
-     * 暂停
+     * 
      *
      * @param progress
      * @param currentLength
